df6f83ec8a6bcbac96a600a7197b23db8d74e64f
Dennis <76845672+DennisNeu@users.noreply.github.com>
2024-11-05 14:47:52 +0100

    Implent ship class

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Overall class to manage game assets and behaviour"""
@@ -21,6 +22,8 @@ class AlienInvasion:
         
         self.bg_color = (230, 230, 230)
 
+        self.ship = Ship(self)
+
     def run_game(self):
         """Start main game loop"""
         while True:
@@ -34,7 +37,9 @@ class AlienInvasion:
 
             # Redraw bg_color
             self.screen.fill(self.settings.bg_color)
-        
+            
+            self.ship.blitme()
+
             # Make the most recently drawn screen visible    
             pygame.display.flip()
             self.clock.tick(self.settings.fps)
---
+++ ship.py
@@ -0,0 +1,21 @@
+import pygame
+
+class Ship:
+    """A calss to manage the space ship"""
+
+    def __init__(self, ai_game):
+        """Initialize the ship and set starting position"""
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        # Load the image and get its rect
+        self.image = pygame.image.load('img/ship.bmp')
+        self.rect = self.image.get_rect()
+
+        # Place at bottom center of screen
+        self.rect.midbottom = self.screen_rect.midbottom
+
+    def blitme(self):
+        """Draw the ship at current location"""
+        self.screen.blit(self.image, self.rect)
+
