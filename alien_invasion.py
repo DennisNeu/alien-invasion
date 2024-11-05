ee207c6b418b423f201c53f62b417995e80a8ca4
Dennis <76845672+DennisNeu@users.noreply.github.com>
2024-11-05 14:22:19 +0100

    added fps to settings class

--- alien_invasion.py
+++ alien_invasion.py
@@ -37,7 +37,7 @@ class AlienInvasion:
         
             # Make the most recently drawn screen visible    
             pygame.display.flip()
-            self.clock.tick(60)
+            self.clock.tick(self.settings.fps)
     
 if __name__ == '__main__':
     # Make a game instance and run the game
