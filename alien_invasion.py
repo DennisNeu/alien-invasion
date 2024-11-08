71ea536aa5bdc07cd1fa7295e63f4d5a1fd267f2
Dennis <76845672+DennisNeu@users.noreply.github.com>
2024-11-08 17:38:26 +0100

    Limit number of bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -80,8 +80,10 @@ class AlienInvasion:
         pygame.display.flip()
 
     def _fire_bullet(self):
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
 
 if __name__ == '__main__':
     # Make a game instance and run the game
